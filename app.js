419404c131ef4804eb4df073fd818bfe1b525248
MH020 <martin98hjelm@proton.me>
2026-09-17 11:55:31 +0200

    setup of basic app

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+
+const app = express()
+//the standard / route send as a javascript object 
+// which is automatically sent as json
+app.get('/', (req, res) => {
+  res.send({ data: 'hello' })
+})
+
+
+
+//this should be the last route in the entire application. 
+// *splat means all routes after this block results to this
+app.all('/{*splat}', (req, res) => {
+  res.status(404).send({ data: "Didn't match with a route" })
+})
+
+
+//port of the application 
+const PORT = 8080
+
+//starts the app on the port 
+app.listen(PORT, () => {
+  console.log('Server is running on port:', PORT)
+})
+
